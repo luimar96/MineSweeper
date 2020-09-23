a0a985b91110773fcf84911ad93a58388c4ab867
Luisa Mårtensson <luisa_martensson@hotmail.com>
2020-09-23 11:35:47 +0200

    Co-authored-by: Philip ryström <prys45@hotmail.com>

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace MineSweeper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          char [,] board =new char[10,10];
+          for (int row = 0; row < 10; row++)
+          {
+              System.Console.Write(row+ " | ");
+              for (int col = 0; col < 10; col++)
+              {
+                   System.Console.Write(board[row, col]);
+                    System.Console.Write(" | ");
+              }
+              System.Console.WriteLine();
+          }
         }
     }
 }
