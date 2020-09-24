9614e5916474a62c085368330eabfa175a168a43
Luisa Mårtensson <luisa_martensson@hotmail.com>
2020-09-24 10:33:17 +0200

    uppdate commit

--- Program.cs
+++ Program.cs
@@ -7,18 +7,36 @@ namespace MineSweeper
         static void Main(string[] args)
         {
           char [,] board =new char[10,10];
-          System.Console.WriteLine("    A   B   C   D   E   F   G   H   I   J");
-           System.Console.WriteLine("+------------------------------------------");
+
+             //initialize
           for (int row = 0; row < 10; row++)
           {
-              System.Console.Write(row+ " | ");
               for (int col = 0; col < 10; col++)
               {
-                   System.Console.Write(board[row, col]);
-                    System.Console.Write(" | ");
+                  board[row,col]='X';
               }
-              System.Console.WriteLine();
+              
+          }
+          
+         
+          
+          Console.WriteLine("  A B C D E F G H I J");
+           System.Console.WriteLine("+---------------------");
+          for (int row = 0; row < 10; row++)
+          {
+              
+              Console.Write(row+" | ");
+              for (int col = 0; col < 10; col++)
+              {
+                   
+                   Console.Write(board[row, col]);
+                    Console.Write(" ");
+
+              }
+              Console.WriteLine();
           }
         }
+
+
     }
 }
